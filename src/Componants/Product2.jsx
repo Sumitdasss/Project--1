aaa4923f4f736f4f129e757e383f1fa5099f473a
Sumit Das <sumit@example.com>
2026-03-30 17:40:53 +0600

    add

--- src/Componants/Product2.jsx
+++ src/Componants/Product2.jsx
@@ -41,15 +41,13 @@ beforeChange: (prev, next) => {
     <div className="flex flex-col items-center gap-2 cursor-pointer group">
      
       <span className={`text-[16px] font-medium transition-all duration-300 ${
-        i === dotActive ? "" : "text-gray-400"
-      }`}>
+        i === dotActive ? "" : "text-gray-400"}`}>
         0{i + 1}
       </span>
       
       
       <div className={`w-[3px] -mt-8 ml-7 h-[30px] transition-all duration-300 ${
-        i === dotActive ? "bg-black" : "bg-gray-200"
-      }`}></div>
+        i === dotActive ? "bg-black" : "bg-gray-200" }`}></div>
     </div>
   ),
 
