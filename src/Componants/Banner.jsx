45e7be287876a16f53ead5f8e97f912ba16e93c3
Sumit Das <sumit@example.com>
2026-03-31 18:23:11 +0600

    ad

--- src/Componants/Banner.jsx
+++ src/Componants/Banner.jsx
@@ -62,8 +62,9 @@ beforeChange: (prev, next) => {
   
   return (
     <>
-    <SlickSlider {...settings}>
+    
       <div className="">
+        <SlickSlider {...settings}>
     <div className='bg-[url(/src/assets/Image.jpg)] bg-cover bg-no-repeat bg-center h-[600px]'>
 
 
@@ -83,30 +84,6 @@ beforeChange: (prev, next) => {
 </div>
 
     </div>
-    <div className="border-t-3 border-b-3 border-[#F0F0F0]">
-<div className="max-w-[1440px] mx-auto   ">
-<div className="flex items-center justify-between  h-[100px]">
-  <div className="flex items-center">
-    <Iconone/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Two years warranty</p>
-  </div>
-  <div className="flex items-center">
-    <Icontwo/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Free shipping</p>
-  </div>
-  <div className="flex items-center">
-    <Iconthree/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Return policy in 30 days</p>
-  </div>
-  
-</div>
-
-</div>
-</div>
-
-    </div>
-   
-      <div className="">
     <div className='bg-[url(/src/assets/Image.jpg)] bg-cover bg-no-repeat bg-center h-[600px]'>
 
 
@@ -126,30 +103,6 @@ beforeChange: (prev, next) => {
 </div>
 
     </div>
-    <div className="border-t-3 border-b-3 border-[#F0F0F0]">
-<div className="max-w-[1440px] mx-auto   ">
-<div className="flex items-center justify-between  h-[100px]">
-  <div className="flex items-center">
-    <Iconone/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Two years warranty</p>
-  </div>
-  <div className="flex items-center">
-    <Icontwo/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Free shipping</p>
-  </div>
-  <div className="flex items-center">
-    <Iconthree/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Return policy in 30 days</p>
-  </div>
-  
-</div>
-
-</div>
-</div>
-
-    </div>
-   
-      <div className="">
     <div className='bg-[url(/src/assets/Image.jpg)] bg-cover bg-no-repeat bg-center h-[600px]'>
 
 
@@ -169,30 +122,6 @@ beforeChange: (prev, next) => {
 </div>
 
     </div>
-    <div className="border-t-3 border-b-3 border-[#F0F0F0]">
-<div className="max-w-[1440px] mx-auto   ">
-<div className="flex items-center justify-between  h-[100px]">
-  <div className="flex items-center">
-    <Iconone/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Two years warranty</p>
-  </div>
-  <div className="flex items-center">
-    <Icontwo/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Free shipping</p>
-  </div>
-  <div className="flex items-center">
-    <Iconthree/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Return policy in 30 days</p>
-  </div>
-  
-</div>
-
-</div>
-</div>
-
-    </div>
-   
-      <div className="">
     <div className='bg-[url(/src/assets/Image.jpg)] bg-cover bg-no-repeat bg-center h-[600px]'>
 
 
@@ -212,30 +141,6 @@ beforeChange: (prev, next) => {
 </div>
 
     </div>
-    <div className="border-t-3 border-b-3 border-[#F0F0F0]">
-<div className="max-w-[1440px] mx-auto   ">
-<div className="flex items-center justify-between  h-[100px]">
-  <div className="flex items-center">
-    <Iconone/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Two years warranty</p>
-  </div>
-  <div className="flex items-center">
-    <Icontwo/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Free shipping</p>
-  </div>
-  <div className="flex items-center">
-    <Iconthree/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Return policy in 30 days</p>
-  </div>
-  
-</div>
-
-</div>
-</div>
-
-    </div>
-   
-      <div className="">
     <div className='bg-[url(/src/assets/Image.jpg)] bg-cover bg-no-repeat bg-center h-[600px]'>
 
 
@@ -255,30 +160,54 @@ beforeChange: (prev, next) => {
 </div>
 
     </div>
-    <div className="border-t-3 border-b-3 border-[#F0F0F0]">
-<div className="max-w-[1440px] mx-auto   ">
-<div className="flex items-center justify-between  h-[100px]">
-  <div className="flex items-center">
-    <Iconone/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Two years warranty</p>
-  </div>
-  <div className="flex items-center">
-    <Icontwo/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Free shipping</p>
-  </div>
-  <div className="flex items-center">
-    <Iconthree/>
-    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Return policy in 30 days</p>
-  </div>
-  
-</div>
+    <div className='bg-[url(/src/assets/Image.jpg)] bg-cover bg-no-repeat bg-center h-[600px]'>
 
-</div>
+
+<div className="max-w-[1440px] mx-auto ">
+  <div className="ml-[170px]">
+  <p className='text-[50px] font-bold text-[#262626] pt-[170px]'>Final Offer</p>
+  <div className="flex items-center gap-x-3 ">
+
+    <p className='text-[23px] font-normal text-[#6D6D6D]'>Up to</p>
+    <p className='text-[60px] font-bold text-[#262626]'>50%</p>
+    <p className='text-[23px] font-normal text-[#6D6D6D]'>sale for all furniture items!</p>
+  </div>
+
+
+  <button className='px-7 py-3 bg-[#262626] text-white font-medium mt-4'>Shop Now</button>
+  </div>
 </div>
 
     </div>
-   
     </SlickSlider>
+    <div className="border-t-3 border-b-3 border-[#F0F0F0]">
+<div className="max-w-[1440px] mx-auto   ">
+<div className="flex items-center justify-between  h-[100px]">
+  <div className="flex items-center">
+    <Iconone/>
+    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Two years warranty</p>
+  </div>
+  <div className="flex items-center">
+    <Icontwo/>
+    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Free shipping</p>
+  </div>
+  <div className="flex items-center">
+    <Iconthree/>
+    <p className='text-[20px] font-normal text-[#6D6D6D] pl-[15px]'>Return policy in 30 days</p>
+  </div>
+  
+</div>
+
+</div>
+</div>
+
+    </div>
+   
+    
+   
+   
+   
+   
     </>
   )
 }
